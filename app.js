3315023b769b9e8689adf23bad031d474b212f77
roman shteynberg <rys495@gmail.com>
2021-02-01 21:25:53 -0500

    second commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+module.exports = app;
+
+app.get('/', (req, res) => {
+    res.send(
+        `
+        <html>
+            <body>
+                Success
+            </body>
+        </html>
+        `
+    );
+});
